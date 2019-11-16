23de51a40e832666e892feb28308bb4ed7fe1a1e
Nicole Watrinet <niciwatri@hotmail.com>
2019-11-16 10:26:16 +0100

    Dateien hinzufügen

---
+++ components 2/skill.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  	<h3 class="Skill_title">{{ title }}</h3>
+  	<div class="background">
+  		<div class="active" :style="{ width: level + '%' }">{{ level }} %</div>
+  	</div>
+  </div>
+</template>
+
+<script>
+  export default {
+  	props: ['title', 'level']
+  }
+</script>
+
+<style>
+ .background {
+ 	background-color: gray;
+ }
+ .active {
+ 	background-color: green;
+ }
+ .Skill_title {
+ 	margin-top: 3rem;
+ }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1>Hallo</h1>
+    <Skill title="Design" level="90"/>
+    <Skill title="Technik" level="60"/>
+    <Skill title="Konzept" level="80"/>
+  </div>
+</template>
+
+<script>
+import Skill from '~/components/skill.vue'
+
+export default {
+  components: {
+    Skill
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  max-width: 38rem;
+}
+</style>
